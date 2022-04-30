744681334772d4916978ed131a76bccde289070d
xtremeevan <80179007+xtremeevan@users.noreply.github.com>
2022-04-30 16:31:49 -0500

    h2

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ function Header() {
    return (
       <header>
          <h1>
-            The<strong>AnimeList</strong>guide
+            The<strong>AnimeLookup</strong>Guide
+            <h2>Look up an Anime get get started</h2>
          </h1>
       </header>
    );
